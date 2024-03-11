81509dd05fdcee105568f7cf8fd6b14f836296e6
Yash jain <jyash5332@gmail.com>
2024-03-11 20:36:06 +0530

    calculator

--- Day 1 Git Groove-Mastering Collaboration on GitHub/Calculator-JS/script.js
+++ Day 1 Git Groove-Mastering Collaboration on GitHub/Calculator-JS/script.js
@@ -1,1 +1,23 @@
-// Do the logic part by yourself
+let btn = document.querySelector(".buttons");
+let value = document.querySelector("#value");
+let span = document.querySelectorAll('span');
+let togglebtn = document.querySelector(".togglebtn");
+let body = document.querySelector('body');
+
+for(let i=0; i<span.length; i++){
+    span[i].addEventListener("click", function(){
+        if(this.innerHTML=="="){
+            value.innerHTML=eval(value.innerHTML);
+        }else{
+            if(this.innerHTML=="Clear"){
+                value.innerHTML="";
+            }else{
+                value.innerHTML+=this.innerHTML;
+            }
+        }
+    })
+}
+
+togglebtn.onclick = function(){
+    body.classList.toggle('dark')
+}
